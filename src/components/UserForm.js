519a1f6af8a15a868c7d7ab15ecac36485031195
ATirado <azuru5039@gmail.com>
2024-06-01 16:00:24 -0400

    Add UserForm component with initial state and methods

---
+++ src/components/UserForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+    }
+
+    // Proceed to next step
+    nextStep = () => {
+        const { step } = this.state
+        this.setState({
+            step: step + 1
+        })
+    }
+
+    // Go back to prev step
+    prevStep = () => {
+        const { step } = this.state
+        this.setState({
+            step: step - 1
+        })
+    }
+
+    // Handle fields change
+    handleChange = input => e => {
+        this.setState({ [input]: e.target.value })
+    }
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default UserForm
